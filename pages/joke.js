7fb1b5d39e1b72355bf0f756984804022159781a
Jordan Whistler <whistler.5@osu.edu>
2023-02-15 11:13:53 -0500

    testing random thing

---
+++ pages/joke.js
@@ -0,0 +1,33 @@
+export default function Joke({ joke, status }) {
+  return (
+    <div>
+      {status}
+      <JokeBlock joke={joke} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json", UserAgent: "testing-Netlify" },
+  });
+  const res2 = await fetch("/");
+  if (!res.ok) {
+    return {
+      props: {
+        joke: "No joke for you.",
+      },
+      revalidate: 60,
+    };
+  }
+
+  const { joke } = await res.json();
+
+  return {
+    props: {
+      joke: joke,
+      status: res2.status,
+    },
+    revalidate: 60,
+  };
+}
